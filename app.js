2d91e796d7e68e045b6fa2ddd80c0cdb4c46e714
kirannarayanks <kirannarayanks@gmail.com>
2023-01-06 18:25:37 +0530

    first upload

---
+++ app.js
@@ -0,0 +1,31 @@
+const newslide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navlinks = document.querySelectorAll('.nav-links li')
+
+    burger.addEventListener('click',() => {
+        nav.classList.toggle('nav-active');
+
+        //animate links
+        navlinks.forEach((link,index) => {
+            if(link.style.animation){
+                link.style.animation = '';
+
+            }
+            else {
+                link.style.animation = 'nav 0.5s ease forwrds ${index / 7 +1.5s}s'
+            }
+
+        });
+        //burger animation
+        
+        burger.classList.toggle('toggle');
+
+    });
+}
+newslide();
+
+window.addEventListener('load', () => {
+    const preload = document.querySelector('.preloader');
+    preload.classList.add('preload-finish');
+})
